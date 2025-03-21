282a63afb3f6e8e80e7dbaadb4a4a778b69eefbb
PAST666 <alex66651@yandex.ru>
2025-03-21 22:06:17 +0800

    создание моделей (сообщение и получатель)

--- mailings/models.py
+++ mailings/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+MAX_NAME_LENGTH = 150
+MAX_TEXT_LENGTH = 255
+
+class message(models.Model):
+    message_title = models.CharField('Заголовок', max_length=MAX_NAME_LENGTH)
+    message_text = models.CharField('Текст сообщения', max_length=MAX_TEXT_LENGTH)
+
+    class Meta:
+        verbose_name = 'Сообщение'
+        verbose_name_plural = 'Сообщения'
+    
+    def __str__(self):
+        return self.name
+
+class message_recipient(models.Model):
+    email = models.EmailField('Почта', max_length=MAX_NAME_LENGTH, unique=True)
+    full_name = models.CharField('ФИО', max_length=MAX_NAME_LENGTH)
+    comment = models.TextField('Комментарий', max_length=MAX_TEXT_LENGTH)
+
+    class Meta:
+        verbose_name = 'Получатель сообщения'
+        verbose_name_plural = 'Получатели сообщений'
+    
+    def __str__(self):
+        return self.name    
+
