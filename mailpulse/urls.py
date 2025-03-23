e87f3d27b759132200776a48edcc46eb86958c71
PAST666 <alex66651@yandex.ru>
2025-03-23 12:36:06 +0800

    приложение users(urls, views, templats)

--- mailpulse/urls.py
+++ mailpulse/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from mailings.views import MainView
+from users import urls
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', MainView.as_view(), name='main')
+    path('', MainView.as_view(), name='main'),
+    path('users/', include(urls)),
 ]
---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+
+
+urlpatterns = [
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+]
