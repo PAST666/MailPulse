e844f6c5ad6acf389fd2ba6a4ffac935620acba8
PAST666 <alex66651@yandex.ru>
2025-03-29 21:52:32 +0800

    apps

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from .views import CustomLoginView, CustomLogoutView, CustomRegisterView, ProfileUser, VerifyEmailView
 
+app = 'users'
+
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
