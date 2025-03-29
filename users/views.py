3bed48da91ca6b32bfdeec0b5d301a09de6c6a70
PAST666 <alex66651@yandex.ru>
2025-03-29 21:35:25 +0800

    views.py

--- users/views.py
+++ users/views.py
@@ -1,16 +1,16 @@
+import uuid
+
+from django.conf import settings
+from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.mail import send_mail
 from django.views.generic import CreateView, TemplateView
 from django.urls import reverse_lazy
 from django.views.generic.edit import UpdateView
-from django.shortcuts import redirect, get_object_or_404
-from django.contrib import messages
-from django.utils.timezone import now
-from datetime import timedelta
-
 from .forms import CustomLoginForm, CustomUserCreationForm, ProfileUserForm
-from users.models import User
-from .utils import send_verification_email
+from users.models import User, ActivationToken
+
 
 class CustomLoginView(LoginView):
     form_class = CustomLoginForm
@@ -25,48 +25,31 @@ class CustomLogoutView(LogoutView):
 class CustomRegisterView(CreateView):
     form_class = CustomUserCreationForm
     template_name = 'users/register.html'
-    success_url = reverse_lazy('users:email_verification_sent')
-    
+    success_url = reverse_lazy('password_reset_sent')
+
     def form_valid(self, form):
-        # Сохраняем пользователя, но делаем его неактивным до подтверждения email
-        user = form.save(commit=False)
-        user.is_active = False  # Пользователь будет активирован после подтверждения email
+        user = form.save()
+        user.is_active = False
         user.save()
-        
-        # Отправляем письмо с подтверждением
-        send_verification_email(user, self.request)
-        
+
+        token = ActivationToken.objects.create(user=user)
+        verification_url = self.request.build_absolute_uri(
+            reverse_lazy(
+                'password_reset_confirm',
+                kwargs={"token": str(token.token)}
+            ),
+        )
+
+        send_mail(
+            "Подтверждение регистрации в MailPulse",
+            f"Для активации перейдите по ссылке: {verification_url}",
+            settings.EMAIL_HOST_USER,
+            [user.email],
+            fail_silently=False,
+        )
+
         return super().form_valid(form)
 
-class EmailVerificationSentView(TemplateView):
-    template_name = 'users/email_verification_sent.html'
-    extra_context = {'title': 'Подтверждение email'}
-
-class VerifyEmailView(TemplateView):
-    template_name = 'users/email_verified.html'
-    
-    def get(self, request, *args, **kwargs):
-        token = kwargs.get('token')
-        try:
-            user = get_object_or_404(User, email_verification_token=token)
-            
-            # Проверяем, не истек ли срок действия токена (например, 24 часа)
-            token_expiry = user.email_verification_token_created + timedelta(hours=24)
-            if now() > token_expiry:
-                messages.error(request, 'Срок действия ссылки истек. Пожалуйста, зарегистрируйтесь заново.')
-                return redirect('users:register')
-            
-            # Активируем пользователя
-            user.is_active = True
-            user.is_email_verified = True
-            user.save()
-            
-            messages.success(request, 'Ваш email успешно подтвержден! Теперь вы можете войти в систему.')
-            return redirect('login')
-            
-        except Exception as e:
-            messages.error(request, 'Неверная ссылка подтверждения.')
-            return redirect('users:register')
 
 class ProfileUser(LoginRequiredMixin, UpdateView):
     model = User
@@ -77,14 +60,34 @@ class ProfileUser(LoginRequiredMixin, UpdateView):
     def get_success_url(self) -> str:
         return reverse_lazy('users:profile', args=[self.request.user.pk])
 
+class EmailVerificationSendView(TemplateView):
+    template_name = 'users/password_reset_sent.html'
 
 
+class VerifyEmailView(TemplateView):
+    template_name = 'users/email_verified.html'
 
+    def get(self, request, *args, **kwargs):
+        try:
+            token = ActivationToken.objects.get(token=self.kwargs.get('token'),)
+        except ActivationToken.DoesNotExist:
+            return self.render_to_response(
+                {"error": "Недействительная ссылка"}
+            )
 
+        # TODO: Нужно реализовать логику проверки: Не истек ли срок действия токена
 
-# TODO переопределить метод form_valid
+        user = token.user
+        user.is_active = True
+        user.save()
+
+        token.delete()  # После активации пользователя удаляется токен
+        login(request, user)  # авторизуется пользователь
+
+        return super().get(request, *args, **kwargs)
+
+    # TODO переопределить метод form_valid
 # TemplateView
-# TODO реализовать класс EmailVerificationSendView - страница подтверждения письма
 # TODO реализовать класс VerifyEmailView - подтверждение
 # TODO реализовать класс ProfileView для просмотра и редактирования профиля
 
