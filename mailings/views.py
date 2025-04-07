594b6683b57df1827eeac2d6b6d865f5122c705b
PAST666 <alex66651@yandex.ru>
2025-04-07 18:57:43 +0800

    начало логики views_mailings

--- mailings/views.py
+++ mailings/views.py
@@ -1,7 +1,10 @@
+from django.contrib.auth.models import Group, User
+from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render
-from django.views.generic import View
+from django.views.generic import View, ListView
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from .models import Mailing, Message, Recipient, MailingStatus
 
-class MainView(View):
-    def get(self, request):
-        return render(request, 'mailings/main.html')
 
+manager_group = Group.objects.get(name='Менеджеры')
+manager_group_members = manager_group.user_set.all()
